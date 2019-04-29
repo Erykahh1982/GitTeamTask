a1302e0eb7dae308d38ef52caf9e3d1d1a1a5d19
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2019-04-29 13:59:17 +0200

    a1

--- src/Greet.java
+++ src/Greet.java
@@ -1,20 +1,7 @@
 public class Greet {
 
-    public String name;
-
-    public Greet(String name) {
-        this.name = name;
+    public void printYourName() {
+        System.out.println("Erika");
     }
 
-    public void printYourName(String name) {
-        System.out.println(name);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
